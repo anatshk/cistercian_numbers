0a4ff78999d21918a5eb53b270c1b2503bb7fa86
AnatShk <curgen@gmail.com>
2021-02-05 08:56:09 +0200

    added some more failing tests

--- tests/test_translating_cistercian_symbols.py
+++ tests/test_translating_cistercian_symbols.py
@@ -1,6 +1,7 @@
 import unittest
 import numpy as np
 
+from symbol_generation.symbol_classes import CistercianSymbol
 from symbol_generation.symbol_mapping import create_symbols
 from translating_cistercian_symbols import CistercianNumber, SYMBOL_WIDTH, SYMBOL_HEIGHT, arabic_to_cistercian
 
@@ -95,31 +96,105 @@ class TestArabicToCistercian(unittest.TestCase):
         np.testing.assert_array_equal(cistercian.get_symbol(), expected_symbol)
 
     def test_zero(self):
-        # TODO: number = 0
-        self.assertTrue(False)
+        number = 0
+        cistercian = arabic_to_cistercian(number)
+        expected_symbol = np.array([
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+        ])
+        self.assertEqual(cistercian.value, number)
+        np.testing.assert_array_equal(cistercian.get_symbol(), expected_symbol)
 
     def test_number_out_of_range(self):
-        # TODO: number < 0 or number > 9999 - raise exception
-        self.assertTrue(False)
+        with self.assertRaisesRegex(Exception, "Number out of range"):
+            arabic_to_cistercian(-5)
+
+        with self.assertRaisesRegex(Exception, "Number out of range"):
+            arabic_to_cistercian(10342)
+
+    def test_number_not_an_int(self):
+        with self.assertRaisesRegex(Exception, "Unsupported input, only int supported, got"):
+            arabic_to_cistercian(5.3)
 
 
 class TestCistercianToArabic(unittest.TestCase):
-    def test_2_symbol(self):
-        # TODO: select single symbol and test
-        self.assertTrue(False)
+    def test_1_symbol(self):
+        # single symbol - 1000
+        symbol = np.array([
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [1, 1, 1, 0, 0],
+        ])
+        symbol_instance = CistercianSymbol(height=SYMBOL_HEIGHT, width=SYMBOL_WIDTH)
+        symbol_instance.set_symbol(symbol)
+        number = CistercianNumber(height=SYMBOL_HEIGHT, width=SYMBOL_WIDTH)
+        number.add_symbol(symbol_instance)
+
+        expected_number = CistercianNumber(height=SYMBOL_HEIGHT, width=SYMBOL_WIDTH)
+        expected_number.add_symbol(SYMBOL_MAPPING[1000])
+        self.assertEqual(expected_number, number)
 
     def test_2_symbols(self):
-        # TODO: select 2 symbols combination and test
-        self.assertTrue(False)
+        # two symbols - 1000 + 30
+        symbol = np.array([
+            [0, 0, 1, 0, 0],
+            [0, 1, 1, 0, 0],
+            [1, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [1, 1, 1, 0, 0],
+        ])
+        symbol_instance = CistercianSymbol(height=SYMBOL_HEIGHT, width=SYMBOL_WIDTH)
+        symbol_instance.set_symbol(symbol)
+        number = CistercianNumber(height=SYMBOL_HEIGHT, width=SYMBOL_WIDTH)
+        number.add_symbol(symbol_instance)
+
+        expected_number = CistercianNumber(height=SYMBOL_HEIGHT, width=SYMBOL_WIDTH)
+        expected_number.add_symbol(SYMBOL_MAPPING[1000])
+        expected_number.add_symbol(SYMBOL_MAPPING[30])
+        self.assertEqual(expected_number, number)
 
     def test_3_symbols(self):
-        # TODO: select 3 symbols combination and test
-        self.assertTrue(False)
+        # 3 symbols - 1000 + 30 + 700
+        symbol = np.array([
+            [0, 0, 1, 0, 0],
+            [0, 1, 1, 0, 0],
+            [1, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 1],
+            [1, 1, 1, 1, 1],
+        ])
+        symbol_instance = CistercianSymbol(height=SYMBOL_HEIGHT, width=SYMBOL_WIDTH)
+        symbol_instance.set_symbol(symbol)
+        number = CistercianNumber(height=SYMBOL_HEIGHT, width=SYMBOL_WIDTH)
+        number.add_symbol(symbol_instance)
+
+        expected_number = CistercianNumber(height=SYMBOL_HEIGHT, width=SYMBOL_WIDTH)
+        expected_number.add_symbol(SYMBOL_MAPPING[1000])
+        expected_number.add_symbol(SYMBOL_MAPPING[30])
+        expected_number.add_symbol(SYMBOL_MAPPING[700])
+        self.assertEqual(expected_number, number)
 
     def test_4_symbols(self):
         # TODO: select 4 symbols combination and test
+        # 5 symbols - 1000 + 30 + 700 + 5
         self.assertTrue(False)
 
     def test_unknown_symbol(self):
         # TODO: create an unsupported symbol and test - raise exception
+        self.assertTrue(False)
+
+    def tezt_zero(self):
+        # TODO: test with zero symbol (empty array)
         self.assertTrue(False)
