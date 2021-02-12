112e7d86ad630a294271e0f7d73960f69c73b393
AnatShk <curgen@gmail.com>
2021-02-12 18:15:52 +0200

    wip train model

--- read_cistercian_with_cnn/train_model.py
+++ read_cistercian_with_cnn/train_model.py
@@ -2,13 +2,44 @@
 based on TF MNIST beginners tutorial
 https://www.tensorflow.org/tutorials/quickstart/beginner
 """
+import numpy as np
 import tensorflow as tf
 
-from read_cistercian_with_cnn import consts
+from sklearn.model_selection import train_test_split
 
-# MNIST-like model
+from read_cistercian_with_cnn import consts
 from read_cistercian_with_cnn.data_creation import CistercianImageGenerator
 
+# split the data into train, validation and test - split manually to control different sizes in datasets
+# TODO: if all permutations are too large, there may be a memory issue (SIGKILL, exit code 137)
+#  - in that case, consider initializing the data generators with save_intermediate_mappings=False
+#  (either just the largest train generator or all of them)
+
+height_range = np.arange(consts.DATA_MIN_HEIGHT, consts.DATA_MAX_HEIGHT + 1)
+width_range = np.arange(consts.DATA_MIN_WIDTH, consts.DATA_MAX_WIDTH + 1)
+
+
+def split_datasets(array, test_size, train_size, random_state=90210):
+    split = train_test_split(array, test_size=test_size, train_size=train_size, random_state=random_state, shuffle=True)
+    return split[0], split[1]
+
+
+train_height, non_train_height = split_datasets(height_range, test_size=0.4, train_size=0.6)
+validation_height, test_height = split_datasets(non_train_height, test_size=0.5, train_size=0.5)
+train_width, non_train_width = split_datasets(width_range, test_size=0.4, train_size=0.6)
+validation_width, test_width = split_datasets(non_train_width, test_size=0.5, train_size=0.5)
+
+# train data - on all numbers, but part of the available input sizes
+train_data_generator = CistercianImageGenerator(batch_size=consts.BATCH_SIZE, network_input_size=consts.INPUT_SIZE,
+                                                min_value=consts.DATA_MIN_VALUE, max_value=consts.DATA_MAX_VALUE,
+                                                list_of_heights=train_height, list_of_widths=train_width)
+
+# validation data
+validation_data_generator = CistercianImageGenerator(batch_size=consts.BATCH_SIZE, network_input_size=consts.INPUT_SIZE,
+                                                     min_value=consts.DATA_MIN_VALUE, max_value=consts.DATA_MAX_VALUE,
+                                                     list_of_heights=validation_height, list_of_widths=validation_width)
+
+# fit the MNIST-like model
 model = tf.keras.models.Sequential([
   tf.keras.layers.Flatten(input_shape=(consts.INPUT_SIZE, consts.INPUT_SIZE)),
   tf.keras.layers.Dense(consts.DENSE_LAYER_UNITS, activation='relu'),
@@ -18,56 +49,20 @@ model = tf.keras.models.Sequential([
 
 loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 
-model.compile(optimizer='adam',
-              loss=loss_fn,
-              metrics=['accuracy'])
+model.compile(optimizer='adam', loss=loss_fn, metrics=['accuracy'])
 
-# model fit - with a Sequence generator
-# train data - on all numbers, but part of the available input sizes
-train_min_height = consts.DATA_MIN_HEIGHT + 5
-train_max_height = consts.DATA_MAX_HEIGHT - 5
-train_min_width = consts.DATA_MIN_WIDTH + 5
-train_max_width = consts.DATA_MAX_WIDTH - 5
-train_data_generator = CistercianImageGenerator(batch_size=32, network_input_size=consts.INPUT_SIZE,
-                                                min_value=consts.DATA_MIN_VALUE, max_value=consts.DATA_MAX_VALUE,
-                                                min_height=train_min_height, max_height=train_max_height,
-                                                min_width=train_min_width, max_width=train_max_width)
-
-model.fit(x=train_data_generator, epochs=100, validation_split=0.2, shuffle=True, verbose=1)
+model.fit(x=train_data_generator, epochs=consts.EPOCHS, validation_data=validation_data_generator,
+          shuffle=True, verbose=1)
+model.save('cistercian_mnist')
 
 # evaluate on train set - to see if there is over/under fit
 performance_train = model.evaluate(x=train_data_generator, verbose=2, return_dict=True)
 
 # evaluate model on remaining data
 # create data generators for remaining height and width combinations
-test_data_generator_small = CistercianImageGenerator(batch_size=32, network_input_size=consts.INPUT_SIZE,
-                                                     min_value=consts.DATA_MIN_VALUE, max_value=consts.DATA_MAX_VALUE,
-                                                     min_height=consts.DATA_MIN_HEIGHT, max_height=train_min_height,
-                                                     min_width=consts.DATA_MIN_WIDTH, max_width=train_min_width)
-
-test_data_generator_large = CistercianImageGenerator(batch_size=32, network_input_size=consts.INPUT_SIZE,
-                                                     min_value=consts.DATA_MIN_VALUE, max_value=consts.DATA_MAX_VALUE,
-                                                     min_height=train_max_height, max_height=consts.DATA_MAX_HEIGHT,
-                                                     min_width=train_max_width, max_width=consts.DATA_MAX_WIDTH)
-
-test_data_generator_small_height = CistercianImageGenerator(batch_size=32, network_input_size=consts.INPUT_SIZE,
-                                                            min_value=consts.DATA_MIN_VALUE,
-                                                            max_value=consts.DATA_MAX_VALUE,
-                                                            min_height=consts.DATA_MIN_HEIGHT,
-                                                            max_height=train_min_height,
-                                                            min_width=train_max_width, max_width=consts.DATA_MAX_WIDTH)
-
-test_data_generator_small_width = CistercianImageGenerator(batch_size=32, network_input_size=consts.INPUT_SIZE,
-                                                           min_value=consts.DATA_MIN_VALUE,
-                                                           max_value=consts.DATA_MAX_VALUE,
-                                                           min_height=train_max_height,
-                                                           max_height=consts.DATA_MAX_HEIGHT,
-                                                           min_width=consts.DATA_MIN_WIDTH, max_width=train_min_width)
+test_data_generator = CistercianImageGenerator(batch_size=consts.BATCH_SIZE, network_input_size=consts.INPUT_SIZE,
+                                               min_value=consts.DATA_MIN_VALUE, max_value=consts.DATA_MAX_VALUE,
+                                               list_of_heights=test_height, list_of_widths=test_width)
 
 # perform evaluations on each of the test generators
-performance_test_small = model.evaluate(test_data_generator_small, verbose=2, return_dict=True)
-performance_test_large = model.evaluate(test_data_generator_large, verbose=2, return_dict=True)
-performance_test_small_height = model.evaluate(test_data_generator_small_height, verbose=2, return_dict=True)
-performance_test_small_width = model.evaluate(test_data_generator_small_width, verbose=2, return_dict=True)
-
-# TODO: average the losses per test set and see conclusions
+performance_test_small = model.evaluate(test_data_generator, verbose=2, return_dict=True)
