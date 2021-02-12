18d74af50dd5f7f538e979c313d9c49cd0df715d
AnatShk <curgen@gmail.com>
2021-02-12 11:45:00 +0200

    wip on data generator

--- read_cistercian_with_cnn/data_creation.py
+++ read_cistercian_with_cnn/data_creation.py
@@ -15,12 +15,13 @@ from read_cistercian_with_cnn import consts
 
 
 class CistercianImageGenerator(tf.keras.utils.Sequence):
-    def __init__(self, batch_size,
+    def __init__(self, batch_size, network_input_size=consts.INPUT_SIZE,
                  min_value=consts.DATA_MIN_VALUE, max_value=consts.DATA_MAX_VALUE,
                  min_height=consts.DATA_MIN_HEIGHT, max_height=consts.DATA_MAX_HEIGHT,
                  min_width=consts.DATA_MIN_WIDTH, max_width=consts.DATA_MAX_WIDTH):
 
         self.batch_size = batch_size
+        self.network_input_size = network_input_size
         self.min_value = min_value
         self.max_value = max_value
         self.min_height = min_height
@@ -28,8 +29,29 @@ class CistercianImageGenerator(tf.keras.utils.Sequence):
         self.min_width = min_width
         self.max_width = max_width
 
-        self.total_length = (self.max_value - self.min_value + 1) * (self.max_height - self.min_height + 1) \
-            * (self.max_width - self.min_width + 1)
+        self._create_all_permutations()
+
+    def _create_all_permutations(self):
+        numbers = np.arange(self.min_value, self.max_value + 1, 1)
+        height = np.arange(self.min_height, self.max_height + 1, 1)
+        width = np.arange(self.min_width, self.max_width + 1, 1)
+        n, h, w = np.meshgrid(numbers, height, width)
+        self.y = n.flatten()
+        self.x_dims = list(zip(h.flatten(), w.flatten()))
 
     def __len__(self):
-        return math.ceil(self.total_length / self.batch_size)
+        return math.ceil(len(self.y) / self.batch_size)
+
+    def _create_x_arrays_for_training(self, numbers, dimension_tuples):
+        # TODO: create numbers on the fly
+        pass
+
+    def __getitem__(self, idx):
+        batch_x_dims = self.x_dims[idx * self.batch_size:(idx + 1) * self.batch_size]
+        batch_y = self.y[idx * self.batch_size:(idx + 1) * self.batch_size]
+
+        # time <> memory tradeoff - we can create all the symbol mappings in advance and save numpy files
+        # (~36MB per 10K numbers of given size) or create the numpy arrays on the fly
+        batch_x = self._create_x_arrays_for_training(numbers=batch_y, dimension_tuples=batch_x_dims)
+
+        return batch_x, batch_y
--- tests/test_data_creation.py
+++ tests/test_data_creation.py
@@ -1,4 +1,5 @@
 import unittest
+import numpy as np
 
 from read_cistercian_with_cnn.data_creation import CistercianImageGenerator
 
@@ -6,7 +7,7 @@ from read_cistercian_with_cnn.data_creation import CistercianImageGenerator
 class TestCistercianImageGenerator(unittest.TestCase):
     def test_sanity(self):
         cig = CistercianImageGenerator(batch_size=7, min_value=5, max_value=9, min_height=7, max_height=10,
-                                       min_width=5, max_width=10)
+                                       min_width=5, max_width=10, network_input_size=28)
 
         self.assertEqual(cig.batch_size, 7)
         self.assertEqual(cig.min_value, 5)
@@ -15,7 +16,40 @@ class TestCistercianImageGenerator(unittest.TestCase):
         self.assertEqual(cig.max_height, 10)
         self.assertEqual(cig.min_width, 5)
         self.assertEqual(cig.max_width, 10)
-        self.assertEqual(cig.total_length, 5 * 4 * 6)
+        self.assertEqual(cig.network_input_size, 28)
         self.assertEqual(len(cig), 18)
+
+    def test_create_all_permutations(self):
+        cig = CistercianImageGenerator(batch_size=7, min_value=5, max_value=6, min_height=9, max_height=10,
+                                       min_width=8, max_width=10)
+
+        expected_y = np.array([5, 5, 5, 6, 6, 6, 5, 5, 5, 6, 6, 6])
+
+        expected_x_dims = [
+            (9, 8),
+            (9, 9),
+            (9, 10),
+            (9, 8),
+            (9, 9),
+            (9, 10),
+            (10, 8),
+            (10, 9),
+            (10, 10),
+            (10, 8),
+            (10, 9),
+            (10, 10),
+        ]
+
+        np.testing.assert_array_equal(expected_y, cig.y)
+        self.assertEqual(expected_x_dims, cig.x_dims)
+
+    def test_create_x_arrays_for_training(self):
+        cig = CistercianImageGenerator(batch_size=2, network_input_size=16)
+        batch_x, batch_y = cig._create_x_arrays_for_training(numbers=np.array(1, 2004, 389, 54),
+                                                             dimension_tuples=[(7, 5), (17, 15), (30, 50), (7, 5)])
+
+        self.assertEqual(batch_x.shape, (28, 28, 4))
+        self.assertEqual(batch_y.shape, (4, ))
+
 
 
